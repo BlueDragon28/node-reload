9e643debeda0241b24cdf72998b61c445898b3c3
Erwan Saclier de la Bâtie <erwansaclierdelabatie@yahoo.fr>
2022-11-03 11:14:57 +0100

    starting node and restart on change

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
+const watcher = require("./watcher");
 const program = require("./program");
+const node = require("./nodeHandler")();
+
+const files = watcher.find("../testDir");
+
+node.start();
 
 const app = program(() => {
-    console.log("Hello There!");
+    if (watcher.isModified(files)) {
+        console.log("has modification!");
+        node.restart();
+    }
 });
---
+++ nodeHandler.js
@@ -0,0 +1,65 @@
+/*
+This module handle the node starting and destruction.
+*/
+
+const c = require("node:child_process");
+
+class NodeHandler {
+    constructor() {
+        this.processPath = "node";
+        this.fileToStart = "./index.js";
+        this.process = null;
+    }
+
+    /*
+    Starting the node program.
+    */
+    start() {
+        // Stopping the process if its already running.
+        if (this.isRunning()) {
+            this.stop();
+        }
+
+        this.process = c.spawn(this.processPath, [ this.fileToStart ]);
+
+        const output = data => {
+            console.log(`${data}`);
+        }
+
+        this.process.stdout.on("data", output);
+        this.process.stderr.on("data", output);
+
+        this.process.on("close", () => {
+            console.log(`${this.processPath} has been closed!`);
+        })
+    }
+
+    /*
+    Stopping the node program.
+    */
+    stop() {
+        if (this.isRunning()) {
+            this.process.kill();
+            this.process = null;
+        }
+    }
+
+    /*
+    Helper function to restart node.
+    */
+    restart() {
+        this.stop();
+        this.start();
+    }
+
+    /*
+    Indicate if the process is running.
+    */
+    isRunning() {
+        return this.process && this.process.exitCode === null;
+    }
+}
+
+module.exports = function() {
+    return new NodeHandler();
+}
