d6389fb6dc0234d861f701ea3e4ad5d07f003935
Erwan Saclier de la Bâtie <erwansaclierdelabatie@yahoo.fr>
2022-11-07 18:52:10 +0100

    options: adding comments

--- options.js
+++ options.js
@@ -4,6 +4,9 @@ const strParser = require("./strParser");
 // Parsing the command line options to args.
 args.options([
     {
+        /*
+        The arguments to pass to the subcommand, usually the file to execute by node.
+        */
         name: "arguments",
         description: "The arguments to pass to the subcommand",
         defaultValue: [ "./index.js" ],
@@ -19,11 +22,18 @@ args.options([
         }
     },
     {
+        /*
+        The command to execute, usually node.
+        */
         name: "command",
         description: "The command to execute",
         defaultValue: "node"
     },
     {
+        /*
+        The elements (files or folders) to watch for change.
+        Every time a change is detected, the command (ex: node) is restarted.
+        */
         name: "watch",
         description: "Files or folders to watch for changes",
         defaultValue: [],
@@ -40,6 +50,9 @@ args.options([
         }
     },
     {
+        /*
+        The delay between each iteration of watching, 250 is not too fast or too slow.
+        */
         name: "delay",
         description: "The delay between each interval is ms, cannot be under 15 ms",
         defaultValue: 250,
